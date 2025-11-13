708b514a33922680f179a256f835675dc5ef431e
bsamiee <b.samiee93@gmail.com>
2025-11-13 00:21:48 -0600

    remove unused using

--- libs/core/diagnostics/DiagnosticContext.cs
+++ libs/core/diagnostics/DiagnosticContext.cs
@@ -1,7 +1,6 @@
 using System.Diagnostics;
 using System.Diagnostics.Contracts;
 using System.Globalization;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Arsenal.Core.Validation;
 
