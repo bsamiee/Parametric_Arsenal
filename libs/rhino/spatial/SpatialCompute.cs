04680309658f4644430996d83f6f992eb43f8e4c
bsamiee <155845508+bsamiee@users.noreply.github.com>
2025-11-13 22:38:10 -0600

    Fix planar triangulation orientation handling

--- libs/rhino/spatial/SpatialCompute.cs
+++ libs/rhino/spatial/SpatialCompute.cs
@@ -436,6 +436,12 @@ internal static class SpatialCompute {
 
     [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
     private static bool IsInCircumcircle(Point3d a, Point3d b, Point3d c, Point3d p, IGeometryContext context) {
+        double orientation = ((b.X - a.X) * (c.Y - a.Y)) - ((b.Y - a.Y) * (c.X - a.X));
+        double orientationTolerance = context.AbsoluteTolerance * context.AbsoluteTolerance;
+        if (Math.Abs(orientation) <= orientationTolerance) {
+            return false;
+        }
+
         double ax = a.X - p.X;
         double ay = a.Y - p.Y;
         double bx = b.X - p.X;
@@ -443,7 +449,8 @@ internal static class SpatialCompute {
         double cx = c.X - p.X;
         double cy = c.Y - p.Y;
         double det = (((ax * ax) + (ay * ay)) * ((bx * cy) - (by * cx))) + (((bx * bx) + (by * by)) * ((cx * ay) - (cy * ax))) + (((cx * cx) + (cy * cy)) * ((ax * by) - (ay * bx)));
-        return det > context.AbsoluteTolerance;
+        double adjustedDet = orientation > 0.0 ? det : -det;
+        return adjustedDet > context.AbsoluteTolerance;
     }
 
     /// <summary>Computes 2D Voronoi diagram from Delaunay triangulation, returning cell vertices for each input point.</summary>
@@ -455,17 +462,24 @@ internal static class SpatialCompute {
                 Point3d[] circumcenters = new Point3d[triangles.Length];
                 for (int ti = 0; ti < triangles.Length; ti++) {
                     (Point3d a, Point3d b, Point3d c) = (points[triangles[ti][0]], points[triangles[ti][1]], points[triangles[ti][2]]);
-                    double d = 2.0 * ((a.X * (b.Y - c.Y)) + (b.X * (c.Y - a.Y)) + (c.X * (a.Y - b.Y)));
+                    double orientation = ((b.X - a.X) * (c.Y - a.Y)) - ((b.Y - a.Y) * (c.X - a.X));
+                    double orientationTolerance = context.AbsoluteTolerance * context.AbsoluteTolerance;
+                    if (Math.Abs(orientation) <= orientationTolerance) {
+                        circumcenters[ti] = Point3d.Unset;
+                        continue;
+                    }
+
+                    double twoOrientation = 2.0 * orientation;
                     double aSq = (a.X * a.X) + (a.Y * a.Y);
                     double bSq = (b.X * b.X) + (b.Y * b.Y);
                     double cSq = (c.X * c.X) + (c.Y * c.Y);
-                    circumcenters[ti] = new Point3d(((aSq * (b.Y - c.Y)) + (bSq * (c.Y - a.Y)) + (cSq * (a.Y - b.Y))) / d, ((aSq * (c.X - b.X)) + (bSq * (a.X - c.X)) + (cSq * (b.X - a.X))) / d, a.Z);
+                    circumcenters[ti] = new Point3d(((aSq * (b.Y - c.Y)) + (bSq * (c.Y - a.Y)) + (cSq * (a.Y - b.Y))) / twoOrientation, ((aSq * (c.X - b.X)) + (bSq * (a.X - c.X)) + (cSq * (b.X - a.X))) / twoOrientation, a.Z);
                 }
                 Point3d[][] cells = new Point3d[points.Length][];
                 for (int i = 0; i < points.Length; i++) {
                     List<Point3d> cell = [];
                     for (int ti = 0; ti < triangles.Length; ti++) {
-                        if (triangles[ti].Contains(i)) {
+                        if (circumcenters[ti].IsValid && triangles[ti].Contains(i)) {
                             cell.Add(circumcenters[ti]);
                         }
                     }
